690eb1da29b7be1c3b4c82f8f114f5c9c245f7b7
blue64 <steve@blue64.net>
2009-10-21 16:45:49 -0400

    Updated View

--- src/net/blue64/Sudoku.java
+++ src/net/blue64/Sudoku.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
-import android.view.View;
 import android.view.View.OnClickListener;
 
 
