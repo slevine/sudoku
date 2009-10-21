0dec14ecb1000d277953e6340cdf30a494d4420c
blue64 <steve@blue64.net>
2009-10-21 16:54:56 -0400

    added about dialog

---
+++ src/net/blue64/About.java
@@ -0,0 +1,12 @@
+package net.blue64;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
