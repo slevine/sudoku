e0251ec67b640215d3a62b78b9145bc632214ae3
blue64 <steve@blue64.net>
2009-10-21 16:28:56 -0400

    Updated View

--- src/net/blue64/Sudoku.java
+++ src/net/blue64/Sudoku.java
@@ -2,21 +2,41 @@ package net.blue64;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
 
 /**
  * Created by IntelliJ IDEA.
  * User: levinst
  * Date: Oct 21, 2009
  * Time: 3:46:55 PM
- * To change this template use File | Settings | File Templates.
  */
-public class Sudoku extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class Sudoku extends Activity implements OnClickListener {
+
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.about_button:
+                Intent i = new Intent(this, About.class);
+                startActivity(i);
+                break;
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
     }
 }
