945a6eeb995a2c7acdf671406bdd527e6e1cc95c
Steve Levine <steve@blue64.net>
2009-10-21 17:29:39 -0400

    Added Preferences

--- src/net/blue64/Sudoku.java
+++ src/net/blue64/Sudoku.java
@@ -2,6 +2,9 @@ package net.blue64;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
 import android.view.View.OnClickListener;
@@ -15,7 +18,6 @@ import android.view.View.OnClickListener;
  */
 public class Sudoku extends Activity implements OnClickListener {
 
-
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.about_button:
@@ -25,8 +27,32 @@ public class Sudoku extends Activity implements OnClickListener {
         }
     }
 
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                startActivity(new Intent(this, Prefs.class));
+                return true; // More items go here (if any) ... } return false;
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu
+            (Menu
+                    menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onCreate
+            (Bundle
+                    savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         View continueButton = findViewById(R.id.continue_button);
